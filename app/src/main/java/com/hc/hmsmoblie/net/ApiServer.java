0a2a053583e38e75615f9a9f40cc3e8684601caf
ZSQ970509 <1037557632@qq.com>
2018-06-12 14:12:26 +0800

    1.初始化

--- app/src/main/java/com/hc/hmsmoblie/net/ApiServer.java
+++ app/src/main/java/com/hc/hmsmoblie/net/ApiServer.java
@@ -6,7 +6,9 @@ import com.hc.hmsmoblie.bean.json.LoginJs;
 import java.util.List;
 
 import io.reactivex.Observable;
+import io.reactivex.annotations.NonNull;
 import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
 import retrofit2.http.Query;
@@ -18,7 +20,8 @@ import retrofit2.http.Query;
 public interface ApiServer {
     @GET(UrlHelper.BASE_API + "hmsLogin")
     Observable<HttpResponse<LoginJs>> login(@Query("userName") String userName, @Query("userPassWord") String passWord);
+    @FormUrlEncoded
     @POST(UrlHelper.BASE_API + "getCameraList")
-    Observable<HttpResponse<ProjectVideoBean>> getCameraList(@Field("keyword") String keyword, @Field("pageindex") String pageindex
-            ,@Field("pagesize") String pagesize, @Field("sysId") String sysId, @Field("userid") String userid);
+    Observable<HttpResponse<ProjectVideoBean>> getCameraList(@Field ("keyword") String keyword, @Field("pageindex") String pageindex
+            , @Field("pagesize") String pagesize, @Field("sysId") String sysId, @Field("userid") String userid);
 }
---
+++ app/src/main/java/com/hc/hmsmoblie/widget/CustomLoadMoreView.java
@@ -0,0 +1,30 @@
+package com.hc.hmsmoblie.widget;
+
+import com.chad.library.adapter.base.loadmore.LoadMoreView;
+import com.hc.hmsmoblie.R;
+
+/**
+ * Created by Administrator on 2018/6/12.
+ */
+
+public class CustomLoadMoreView  extends LoadMoreView {
+    @Override
+    public int getLayoutId() {
+        return R.layout.view_load_more;
+    }
+
+    @Override
+    protected int getLoadingViewId() {
+        return R.id.load_more_loading_view;
+    }
+
+    @Override
+    protected int getLoadFailViewId() {
+        return R.id.load_more_load_fail_view;
+    }
+
+    @Override
+    protected int getLoadEndViewId() {
+        return R.id.load_more_load_end_view;
+    }
+}
