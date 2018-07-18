515e8de66abb613c0463c557dcc4a55df258a497
yc <563919364@qq.com>
2018-07-18 14:51:37 +0800

    1.登录服务器用户类型id修改

--- app/src/main/java/com/hc/hmsmoblie/bean/type/UserTypeId.java
+++ app/src/main/java/com/hc/hmsmoblie/bean/type/UserTypeId.java
@@ -1,56 +1,40 @@
 package com.hc.hmsmoblie.bean.type;
 
-import com.hc.hmsmoblie.db.UserInfoPref;
-
 /**
  * 登录服务器 的用户类型
  */
 
 public enum UserTypeId {
-    //汇川,市建委,省工程建设网
-    HSM(), MCC(), PECN();
+    //汇川10,省工程建设网20,市建委30
+    HSM(0), PECN(1), MCC(2);
+    private String[] mName = {"HMS平台", "省工程建设网", "市建委"};
+    private String[] mId = {"10", "20", "30"};
+    private int mIndex;
+
+    UserTypeId(int indext) {
+        this.mIndex = indext;
+    }
 
     public static UserTypeId getUserTypeId(int index) {
         switch (index) {
             case 2:
-                return PECN;
-            case 1:
                 return MCC;
+            case 1:
+                return PECN;
             default:
                 return HSM;
         }
     }
 
     public int getIndex() {
-        switch (this) {
-            case PECN:
-                return 2;
-            case MCC:
-                return 1;
-            default:
-                return 0;
-        }
+        return mIndex;
     }
 
     public String getTypeId() {
-        switch (this) {
-            case HSM:
-                return "10";
-            case MCC:
-                return "20";
-            default:
-                return "30";
-        }
+        return mId[mIndex];
     }
 
     public String getTypeName() {
-        switch (this) {
-            case HSM:
-                return "HMS平台";
-            case MCC:
-                return "市建委";
-            default:
-                return "省工程建设网";
-        }
+        return mName[mIndex];
     }
 }
